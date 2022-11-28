abc610a6765b577466ae631de3578b6f483106be
WebDev <webdev@WebDevs-MBP.fritz.box>
2022-11-28 13:10:25 +0100

    fix payload

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,7 @@ function App() {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(sumOfNumbers),
+          body: JSON.stringify({ sum: sumOfNumbers }),
         }
       );
 
