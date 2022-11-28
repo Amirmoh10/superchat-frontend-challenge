315e78a153f4057ab87dd1f8931ec8968537260d
WebDev <webdev@WebDevs-MBP.fritz.box>
2022-11-28 12:28:04 +0100

    refactor disabled styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,7 +12,7 @@ export default function Button({ variant = "primary", ...props }: ButtonProps) {
         "px-4 py-2",
         variant === "primary" && "bg-black text-white",
         variant === "secondary" && "bg-white border",
-        props.disabled && "cursor-default opacity-70",
+        props.disabled && "cursor-not-allowed opacity-40",
       ].join(" ")}
       {...props}
     />
